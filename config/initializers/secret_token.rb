1e2be8cc0a06299514a335a075e22d2d4b72b384
Sridevi <sridevik@thoughtworks.com>
2011-09-09 20:36:20 +0530

    empty application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PairChartApp::Application.config.secret_token = '881fec8ffa9901b342705334b30be21adb19a62a6b75dc4dfc3c92325374e017d5a4e9cebce031d65c98a1c701c725a0ec2eb1ffc899609507c2b6ca9bd9e449'
